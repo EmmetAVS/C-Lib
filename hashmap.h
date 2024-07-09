7ab880df2f2385f84a090f1154f2504acd9563b2
BuildTools <unconfigured@null.spigotmc.org>
2024-07-08 20:02:49 -0700

    first commit

---
+++ hashmap.h
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define keyType int
+#define valueType int
+
+typedef struct hashMapNode {
+    keyType key;
+    valueType value;
+} hashMapNode;
+
+typedef struct Hashmap {
+    int size;
+    hashMapNode *map;
+    void (*add)(struct Hashmap*, keyType, keyType);
+    valueType (*get)(struct Hashmap*, keyType);
+    valueType (*pop)(struct Hashmap*, keyType);
+} Hashmap;
+
+void hashAdd(Hashmap* map, keyType key, valueType value) {
+    int index = hashGetIndex(map->size, key);
+    hashMapNode working = map->map[index];
+    working.value = value;
+    working.key = key;
+}
+valueType hashPop(Hashmap* map, keyType key) {
+    hashMapNode working = map->map[hashGetIndex(map->size, key)];
+    valueType value = working.value;
+    working.value = NULL;
+    working.key = NULL;
+    return value;
+}
+valueType hashGet(Hashmap* map, keyType key) {
+    return map->map[hashGetIndex(map->size, key)].value;
+}
+int hashGetIndex(int size, keyType key) {
+    char keyString[50];
+    sprintf(keyString, "%f", key);
+    char* s = keyString;
+    int n = sizeof(key)/sizeof(keyString[0]);
+    long long p = 31, m = 1e9 + 7;
+    long long hash = 0;
+    long long p_pow = 1;
+    for(int i = 0; i < n; i++) {
+        hash = (hash + (s[i] - 'a' + 1) * p_pow) % m;
+        p_pow = (p_pow * p) % m;
+    }
+    return -hash & (size-1);
+}
+Hashmap* initHash(const int Size) { 
+    Hashmap* hashmap = (Hashmap*)malloc(sizeof(Hashmap));
+    hashmap->size = Size;
+    hashmap->map = (hashMapNode*)malloc(Size * sizeof(hashMapNode));
+    hashmap->add = hashAdd;
+    hashmap->pop = hashPop;
+    hashmap->get = hashGet;
+}
---
+++ linkedlist.h
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define valueType int
+#define UNSET -1
+
+typedef struct linkedListNode {
+    struct linkedListNode* prev;
+    valueType value;
+    struct linkedListNode* next;
+    } linkedListNode;
+
+typedef struct linkedList {
+    linkedListNode head;
+    linkedListNode tail;
+    int size;
+    void (*add)(struct linkedList*, valueType);
+    void (*insert)(struct linkedList*, valueType);
+    valueType (*get)(struct linkedList*, int);
+    valueType (*pop)(struct linkedList*);
+} linkedList;
+
+void linkedListAdd(linkedList* list, valueType value) {
+    if (list->tail.value == UNSET) {
+        list->tail.value = value;
+    } else if (list->head.value == UNSET && list->size == 1) {
+        linkedListNode* newNode = (linkedListNode*)malloc(sizeof(linkedListNode));
+        newNode->value = value;
+        newNode->next = NULL;
+        newNode->prev = &list->head;
+        list->head.value = list->tail.value;
+        list->tail = *newNode;
+    } else {
+        linkedListNode* newNode = (linkedListNode*)malloc(sizeof(linkedListNode));
+        newNode->value = list->tail.value;
+        newNode->next = &list->tail;
+        newNode->prev = list->tail.prev;
+        list->tail.prev->next = newNode;
+        list->tail.prev = newNode;
+        list->tail.value = value;
+    }
+    list->size++;
+}
+void linkedListInsert(linkedList* list, valueType value) {
+    if (list->head.value == UNSET) {
+        list->head.value = value;
+    } else if (list->tail.value == UNSET && list->size == 1) {
+        linkedListNode* newNode = (linkedListNode*)malloc(sizeof(linkedListNode));
+        newNode->value = value;
+        newNode->prev = NULL;
+        newNode->next = &list->tail;
+        list->tail.value = list->head.value;
+        list->head = *newNode;
+    } else {
+        linkedListNode* newNode = (linkedListNode*)malloc(sizeof(linkedListNode));
+        newNode->value = list->head.value;
+        newNode->prev = &list->head;
+        newNode->next = list->head.next;
+        list->head.next->prev = newNode;
+        list->head.next = newNode;
+        list->head.value = value;
+    }
+    list->size++;
+}
+valueType linkedListGet(linkedList* list, int index) {
+    if (index >= list->size) {
+        perror("IndexError: List index out of range");
+        exit(1);
+    } else if (index <= ((list->size)/2)) {
+        linkedListNode* working = &list->head;
+        for (int i = 0; i < index; i++) {
+            working = working->next;
+        }
+        return working->value;
+    } else {
+        linkedListNode* working = &list->tail;
+        for (int i = 0; i < (list->size-index-1); i++) {
+            working = working->prev;
+        }
+        return working->value;
+    }
+}
+valueType linkedListPop(linkedList* list) {
+    valueType toReturn = list->head.value;
+    list->head = *list->head.next;
+    list->head.next->prev = &list->head;
+    list->size -= 1;
+    return toReturn;
+}
+linkedList initLinkedList() {
+    linkedList list;
+    list.head.prev = NULL;
+    list.head.value = UNSET;
+    list.head.next = &list.tail;
+    list.tail.prev = &list.head;
+    list.tail.value = UNSET;
+    list.tail.next = NULL;
+    list.size = 0;
+    list.add = linkedListAdd;
+    list.insert = linkedListInsert;
+    list.get = linkedListGet;
+    list.pop = linkedListPop;
+    return list;
+}
